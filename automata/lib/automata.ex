6345c1cf2f4a2338b7d920d984c58996143f0e52
Thiago Fonseca <thicorfon@gmail.com>
2018-03-15 16:08:56 -0300

    Create main function for dfa

--- automata/lib/automata.ex
+++ automata/lib/automata.ex
@@ -1,18 +1,31 @@
 defmodule Automata do
-  @moduledoc """
-  Documentation for Automata.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Automata.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def apply_dfa(automata, tape_head, current_state) do
   end
+
+  def run_dfa(automata, [], current_state) do
+    if Enum.member?(Map.get(automata,:acceptance_states), current_state) do
+      true
+    else
+      false
+    end
+  end
+
+  def run_dfa(automata, tape, current_state) do
+  # automata = %{:transitions => %{  :q1   => %{'a' => :q1, 'b' => :q2}, 
+  #                                 :q2   => %{'a' => :q2, 'b' => :q1}} ,
+  #             :acceptance_states  => [:q1,:q2],
+  #             :start_state        => :q1 }
+  # tape = ['a','b','a']
+
+    [tape_head | tape_tail] = tape
+    new_state = apply_dfa(automata, tape_head, current_state)
+    run_dfa(automata, tape_tail, new_state)
+  end
+
+  def run_dfa_main(automata,tape) do
+    run_dfa(automata,tape,Map.get(automata, :start_state))
+  end
+
 end
+
