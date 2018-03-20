2a14d230330e41b0c97350f37773e44ed18431fc
Thiago Fonseca <thicorfon@gmail.com>
2018-03-20 18:27:54 -0300

    Fix bugs

--- automata/lib/automata.ex
+++ automata/lib/automata.ex
@@ -12,6 +12,8 @@ defmodule Automata do
     end
   end
 
+#----------------------------------------------------------------------------------------------------
+
   def inList(_, []) do
     false
   end
@@ -24,6 +26,8 @@ defmodule Automata do
     end
   end
 
+#----------------------------------------------------------------------------------------------------
+
   def appendList([],list) do
     list
   end
@@ -32,11 +36,15 @@ defmodule Automata do
     [head|appendList(tail,list)]
   end
 
+#----------------------------------------------------------------------------------------------------
+
   def apply_dfa(automata, tape_head, current_state) do
     transitions = Map.get(automata, :transitions)
     current_state_transitions = Map.get(transitions, current_state)
     Map.get(current_state_transitions, tape_head)
   end
+
+#----------------------------------------------------------------------------------------------------
 
   def run_dfa(automata, [], current_state) do
     if Enum.member?(Map.get(automata,:acceptance_states), current_state) do
@@ -54,21 +62,21 @@ defmodule Automata do
   # tape = ['a','b','a']
     [tape_head | tape_tail] = tape
     new_state = apply_dfa(automata, tape_head, current_state)
-    IO.puts(Enum.join(["Tape:"|tape]))
-    IO.puts(([Atom.to_string(current_state)|[" --"|[tape_head|["--> "|Atom.to_string(new_state)]]]]))
-    IO.puts("\n")
+    # IO.puts(Enum.join(["Tape:"|tape]))
+    # IO.puts(([Atom.to_string(current_state)|[" --"|[tape_head|["--> "|Atom.to_string(new_state)]]]]))
+    # IO.puts("\n")
     run_dfa(automata, tape_tail, new_state)
   end
+
+#----------------------------------------------------------------------------------------------------
 
   def run_dfa_main(automata, tape) do
     run_dfa(automata,tape,Map.get(automata, :start_state))
   end
 
 
-
-
-
-
+#----------------------------------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------------------
 
 
   def filter_states([],_) do
@@ -76,7 +84,7 @@ defmodule Automata do
   end
 
   def filter_states(prospect_list, seen_list) do
-    [prospect_head|prospect_tail] = prospect_tail
+    [prospect_head|prospect_tail] = prospect_list
     if inList(prospect_head, seen_list) do 
       filter_states(prospect_tail,seen_list)
     else
@@ -84,7 +92,7 @@ defmodule Automata do
     end
   end
 
-
+#----------------------------------------------------------------------------------------------------
 
   def get_all_empty_transition_states(automata, list_of_states, seen \\[])
 
@@ -96,26 +104,31 @@ defmodule Automata do
     [states_head|states_tail] = list_of_states
     transitions =  Map.get(automata, :transitions)
     current_state_transitions = Map.get(transitions, states_head)
-    possible_transitions = getDistincts(Map.get(current_state_transitions, :epsilon))
-    filtered_transitions = getDistincts(filter_states(possible_transitions,seen))
-    [filtered_transitions|get_all_transition_states(automata,:epsilon,[filtered_transitions|states_tail], [filtered_transitions|seen])]
+    input_transitions = Map.get(current_state_transitions, :epsilon)
+    if input_transitions != nil do
+      possible_transitions = getDistincts(input_transitions)
+      filtered_transitions = getDistincts(filter_states(possible_transitions,seen))
+      appendList(filtered_transitions,get_all_empty_transition_states(automata, appendList(filtered_transitions,states_tail), appendList(filtered_transitions,seen)))
+    else
+      get_all_empty_transition_states(automata, states_tail, seen)
+    end
+
   end
 
-
-
-  def get_all_non_empty_transition_states(automata, input, list_of_states)
-
-  def get_all_non_empty_transition_states(_,_,[]) do
-    []
-  end
+#----------------------------------------------------------------------------------------------------
 
   def get_all_non_empty_transition_states(automata, input, current_state) do
     transitions =  Map.get(automata, :transitions)
     current_state_transitions = Map.get(transitions, current_state)
-    getDistincts(Map.get(current_state_transitions, input))
+    input_transitions = Map.get(current_state_transitions, input)
+    if input_transitions != nil do
+      getDistincts(input_transitions)
+    else
+      []
+    end 
   end
 
-
+#----------------------------------------------------------------------------------------------------
 
   def run_nfa(automata, [], current_state) do
     if Enum.member?(Map.get(automata,:acceptance_states), current_state) do
@@ -127,26 +140,30 @@ defmodule Automata do
 
   def run_nfa(automata, tape, current_state) do
     [tape_head | tape_tail] = tape
-    possible_input_states = get_all_non_empty_transition_states(automata, input, current_state)
+    possible_input_states = get_all_non_empty_transition_states(automata, tape_head, current_state)
     all_possible_states = getDistincts(appendList(possible_input_states,get_all_empty_transition_states(automata,possible_input_states)))
+    # IO.puts(Enum.join(["Tape:"|tape]))
+    # IO.puts([Atom.to_string(current_state)|[" --"|[tape_head|["--> "|List.to_string(Enum.drop(Enum.map(all_possible_states,fn(s) -> Atom.to_string(s) end), -1))]]]])
+    # IO.puts("\n")
     if all_possible_states == [] do
       false
     else
       apply_run_nfa(automata, tape_tail, all_possible_states)
     end
   end
-  
 
+#----------------------------------------------------------------------------------------------------
 
-  def apply_run_nfa(automata, tape, []) do
+  def apply_run_nfa(_, _, []) do
     false
   end
 
   def apply_run_nfa(automata, tape, list_of_states) do
     [states_head|states_tail] = list_of_states
-    (run_nfa(automata, tape, states_head) OR apply_run_nfa(automata,tape,states_tail))
+    (run_nfa(automata, tape, states_head) or apply_run_nfa(automata,tape,states_tail))
   end
 
+#----------------------------------------------------------------------------------------------------
 
   def run_nfa_main(automata, tape) do
   # automata = %{:transitions => %{ :q1   => %{:epsilon => [:q2], 'b' => [:q3]}, 
@@ -155,7 +172,7 @@ defmodule Automata do
   #             :start_state        => :q1 }
   # tape = ['a','b','a']
     start_state = Map.get(automata,:start_state)
-    possible_starts = getDistincts([start_state|get_all_transition_states(automata, [start_state])
-    apply_run_nfa(automata, tape, possible_starts])
+    possible_starts = getDistincts([start_state|get_all_empty_transition_states(automata, [start_state])])
+    apply_run_nfa(automata, tape, possible_starts)
   end
 end
